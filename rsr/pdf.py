ab9492d2cf0ca8d2c7878b29d4a2836c32bf82eb
cgrima <cyril.grima@gmail.com>
2021-07-16 15:58:07 -0500

    HK analytic integral evaluation reverted back to np.inf, was causing errors in some specific cases like ihghly coherent signals

--- rsr/pdf.py
+++ rsr/pdf.py
@@ -193,7 +193,7 @@ def hk(params, x, data=None, eps=None, method = 'analytic'):
         model = [mpmath.nsum(lambda w: integrand_drumheller(w, i, a, s, mu), [0, np.inf]) 
                 for i in x] # Summation
     else:
-        model = [integrate.quad(integrand, 0., 500, args=(i, a, s, mu), full_output=1)[0]
+        model = [integrate.quad(integrand, 0., np.inf, args=(i, a, s, mu), full_output=1)[0]
                  for i in x] # Integration
     
     model = np.array(model)
