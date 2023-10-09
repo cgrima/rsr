cacbbc528adb55a065610e1f27544dee7a38bcfe
cgrima <cyril.grima@gmail.com>
2023-10-09 12:04:45 -0500

    v1.0.7

--- rsr/__init__.py
+++ rsr/__init__.py
@@ -1,7 +1,7 @@
 """Tools and utilities for running the Radar Statistical Reconnaissance
 """
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __author__ = "Cyril Grima"
 
 __all__ = ["detect", "fit", "invert", "pdf", "run", "utils"]
